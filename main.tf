46cb72071632f482acca41e6934cc870c8410c83
vedant <vedantatugade@gmail.com>
2026-03-06 01:06:49 +0530

    updated subnet configuration

--- main.tf
+++ main.tf
@@ -2,16 +2,24 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "web" {
-  ami = "ami-0f3caa1cf4417e51b"
-  instance_type = "t3.micro"
-  key_name = "my-tf-key"
-
-  tags = {
-  Name = "devops-demo"
+data "aws_subnets" "default" {
+  filter {
+    name   = "default-for-az"
+    values = ["true"]
   }
 }
 
+resource "aws_instance" "web" {
+  ami           = "ami-0f3caa1cf4417e51b"
+  instance_type = "t3.micro"
+  key_name      = "my-tf-key"
+
+  subnet_id = tolist(data.aws_subnets.default.ids)[0]
+
+  tags = {
+    Name = "devops-demo"
+  }
+}
 
 output "ec2_ip" {
   value = aws_instance.web.public_ip
